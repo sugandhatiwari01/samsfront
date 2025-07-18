65d7c37b73ca5c54f4d531185fdb86b583fe2555
Sugandha Tiwari <144129750+sugandhatiwari01@users.noreply.github.com>
2025-07-18 22:24:27 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { User, Heart, ShoppingCart, Search, Menu } from 'lucide-react'; // ✅ correct
 import {Link} from "react-router-dom"; 
 import Home from "./Home";
 
